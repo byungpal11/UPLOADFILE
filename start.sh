4ec8e21d903ab4efb94869f9ae6998e56dbc1bc7
jangbyungmin <byungpal11@gmail.com>
2023-03-16 16:46:43 +0900

    a11

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -cp ./bin:./lib/commons-dbcp-1.4.jar:./lib/jsch-0.1.55.jar:./lib/ojdbc14_g10g102.jar com.doro.itf.UpLoadFile&
+java -cp ./lib/commons-dbcp-1.4.jar:./lib/jsch-0.1.55.jar:./lib/ojdbc14_g10g102.jar:./lib/commons-lang3-3.1.jar:./lib/commons-pool-1.4.jar com.doro.itf.UpLoadFile&
